f3773848bf4d7b0fe227a935973f195a3b03ac59
Javier HM <javierhm2004@gmail.com>
2026-02-11 16:55:53 +0100

    hotfix: fixed socials test

--- __tests__/common/footer.test.js
+++ __tests__/common/footer.test.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react"
-import Footer from "@/components/common/footer"
+import { render, screen } from "@testing-library/react";
+import Footer from "@/components/common/footer";
 
 describe("Footer", () => {
   test("all socials must appear", () => {
@@ -8,6 +8,6 @@ describe("Footer", () => {
 
     expect(screen.getByText(/padelinrio@gmail.com/i)).toBeInTheDocument();
     expect(screen.getByText(/@padel.in.rio/i)).toBeInTheDocument();
-    expect(screen.getByText(/666 66 66 66/i)).toBeInTheDocument();
+    expect(screen.getByText(/696 50 38 98/i)).toBeInTheDocument();
   });
-})
+});
