226f75f005829fc5ad6b608067f0429ed637000c
Javier HM <javierhm2004@gmail.com>
2026-03-01 14:47:07 +0100

    fix: removed delayed render

--- components/asociacion/admin/socios.tsx
+++ components/asociacion/admin/socios.tsx
@@ -2,8 +2,6 @@ import { getAllSocios } from "@/app/actions/socios";
 import SociosClient from "./socios-client";
 
 export default async function Socios() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-
   const socios = await getAllSocios();
   return <SociosClient socios={socios ?? []} />;
 }
