353e6caf260edb661a48a4c625f49061d475a631
Javier HM <javierhm2004@gmail.com>
2026-03-10 17:52:35 +0100

    hotfix: Fixed telegram bot socialItem

--- __tests__/common/footer.test.js
+++ __tests__/common/footer.test.js
@@ -9,7 +9,7 @@ describe("Footer", () => {
     expect(screen.getByText(/padelinrio@gmail.com/i)).toBeInTheDocument();
     expect(screen.getByText(/@padel.in.rio/i)).toBeInTheDocument();
     expect(screen.getByText(/696 50 38 98/i)).toBeInTheDocument();
-    expect(screen.getByText(/t.me\/BonIA/i)).toBeInTheDocument();
+    expect(screen.getByText(/t.me\/Padel_in_Rio_bot/i)).toBeInTheDocument();
   });
 
   test("cookie policy link must appear", () => {
